b9b00cf8a45a00b76ad7620a00d641cc66c0d25f
Gitdmnt <100859051+gitdmnt@users.noreply.github.com>
2023-12-20 18:47:11 +0900

    がちゃ

--- src/components/Gacha.tsx
+++ src/components/Gacha.tsx
@@ -4,9 +4,32 @@ import { useState } from "react";
 type prop = {
   gachaItemLists: string[][];
 }
+const div: React.CSSProperties = {
+
+}
+const ul = {
+  listStyle: "none",
+  height: "4rem",
+  marginBlock: "2rem",
+  marginInline: "1rem",
+  padding: "0",
+};
+const li = {
+  margin: "0",
+  padding: "0",
+};
+const button = {
+  display: "block",
+  margin: "auto",
+  color: "#fff",
+  backgroundColor: "#1d1d1d",
+  border: "none",
+  paddingBlock: "1rem",
+  paddingInline: "2.5rem",
+};
 
 const Gacha: React.FC<prop> = ({ gachaItemLists }) => {
-  const [result, setResult] = useState(["勢いよく", "このガチャを引くべきだ", "今", "ここ", "キミは", "世界の"]);
+  const [result, setResult] = useState(["勢いよく", "このガチャを引くべき", "今", "ここ", "キミは", "世界の"]);
   const roll = () => {
     const result = [];
     gachaItemLists.forEach(e => {
@@ -16,13 +39,13 @@ const Gacha: React.FC<prop> = ({ gachaItemLists }) => {
     setResult(result);
 
   };
-  const sentence = result[4] + result[5] + "ため、" + result[2] + result[3] + "で、" + result[0] + result[1] + "。";
+  const sentence = result[4] + result[5] + "ため、" + result[2] + result[3] + "で、" + result[0] + result[1] + "だろう。";
   return (
-    <div className="gacha">
-      <ul className="result">
-        <li>{sentence}</li>
+    <div style={div}>
+      <ul style={ul}>
+        <li style={li}>{sentence}</li>
       </ul>
-      <button className="button" onClick={() => roll()}>引く</button>
+      <button style={button} onClick={() => roll()}>引く</button>
     </div >
   );
 }
