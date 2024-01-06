b924a5e674093e018725749d27692a52d254637f
Gitdmnt <100859051+gitdmnt@users.noreply.github.com>
2024-01-06 22:43:18 +0900

    時差

--- src/components/RevoClock/RevoTime.tsx
+++ src/components/RevoClock/RevoTime.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 export const RevoTime = () => {
-  const [now, setNow] = useState(Date.now());
   const d = new Date();
+  const [now, setNow] = useState(d.getMilliseconds());
   const midnight = Date.UTC(d.getFullYear(), d.getMonth(), d.getDate());
-  const revo_second_const = 864;
+  const rep_second_const = 864;
   function getTime() {
     const elapsed_time_gregorian_mili = now + 9 * 60 * 60 * 1000 - midnight; //UTC+9
-    const elapsed_time_revo = elapsed_time_gregorian_mili / revo_second_const;
-    const hour = (Math.floor(elapsed_time_revo / 100 / 100).toString()).slice(-1);
-    const minute = ("0" + (Math.floor(elapsed_time_revo / 100) % 100).toString()).slice(-2);
-    const second = ("0" + (Math.floor(elapsed_time_revo) % 100).toString()).slice(-2);
+    const elapsed_time_rep = elapsed_time_gregorian_mili / rep_second_const;
+    const hour = (Math.floor(elapsed_time_rep / 100 / 100).toString()).slice(-1);
+    const minute = ("0" + (Math.floor(elapsed_time_rep / 100) % 100).toString()).slice(-2);
+    const second = ("0" + (Math.floor(elapsed_time_rep) % 100).toString()).slice(-2);
     return [hour, minute, second];
   }
   const [time, setTime] = useState(getTime());
@@ -19,10 +19,11 @@ export const RevoTime = () => {
   useEffect(() => {
     setInterval(
       () => {
-        setNow(Date.now());
+        const d = new Date();
+        setNow(d.getMilliseconds());
         setTime(getTime());
       },
-      revo_second_const
+      rep_second_const
     )
   });
 
