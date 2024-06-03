a4bb69f0f02b6439c6931e9346146d71bca32c12
Gitdmnt <100859051+gitdmnt@users.noreply.github.com>
2024-06-03 21:08:55 +0900

    aa

--- src/components/2048/game.svelte
+++ src/components/2048/game.svelte
@@ -3,6 +3,13 @@
 </script>
 
 <h1>hello from {svelte}</h1>
+<p>
+  canvasを使って何かゲームを作りたい。
+  https://developer.mozilla.org/ja/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Svelte_getting_started
+  https://svelte.dev/repl/434e0b14546747688401e8808c060a23?version=3.47.0
+  https://dnass.github.io/svelte-canvas/examples/resize Local
+  Storageを使ってデータを保存しておくことも学ぶ。
+</p>
 
 <style>
   h1 {
