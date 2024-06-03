be375c81f6afd2f1743dec7f138487d553777454
Gitdmnt <100859051+gitdmnt@users.noreply.github.com>
2024-06-03 21:00:28 +0900

    vault backup: 2024-06-03 21:00:28

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gitdmnt.github.io',
-  integrations: [mdx(), react()]
+  integrations: [mdx(), react(), svelte()]
 });
---
+++ src/components/2048/game.svelte
@@ -0,0 +1,11 @@
+<script>
+  const svelte = "SVELTE";
+</script>
+
+<h1>hello from {svelte}</h1>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
