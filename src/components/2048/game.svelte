d06baae9f313e15679b6529c9c79aa82b45629fe
Gitdmnt <100859051+gitdmnt@users.noreply.github.com>
2024-07-05 01:53:54 +0900

    a

--- src/components/2048/game.svelte
+++ src/components/2048/game.svelte
@@ -2,14 +2,17 @@
   const svelte = "SVELTE";
 </script>
 
-<h1>hello from {svelte}</h1>
-<p>
-  canvasを使って何かゲームを作りたい。
-  https://developer.mozilla.org/ja/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Svelte_getting_started
-  https://svelte.dev/repl/434e0b14546747688401e8808c060a23?version=3.47.0
-  https://dnass.github.io/svelte-canvas/examples/resize Local
-  Storageを使ってデータを保存しておくことも学ぶ。
-</p>
+<main>
+  <h1>hello from {svelte}</h1>
+  <p>
+    canvasを使って何かゲームを作りたい。
+    https://developer.mozilla.org/ja/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Svelte_getting_started
+    https://svelte.dev/repl/434e0b14546747688401e8808c060a23?version=3.47.0
+    https://dnass.github.io/svelte-canvas/examples/resize Local
+    Storageを使ってデータを保存しておくことも学ぶ。
+    3マッチパズルを作ることにした。
+  </p>
+</main>
 
 <style>
   h1 {
